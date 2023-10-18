f509ebaf7f15c6e4824ec70e4ed39f3f2a58270a
noob261 <2740772761@qq.com>
2023-10-18 10:23:56 +0800

    project 7 pass all the local tests

--- 07/VMTranslator.py
+++ 07/VMTranslator.py
@@ -17,11 +17,29 @@ ARITHMETIC_OPS.add("not")
 def getComparisonCode(cond, op, idx):
     return f'@SP\nM=M-1\nA=M\nD=M\n@SP\nM=M-1\nA=M\n{cond}\n@COND{idx}\nD;{op}\n@SP\nA=M\nM=0\n@SP\nM=M+1\n@ELSE{idx}\n0;JMP\n(COND{idx})\n@SP\nA=M\nM=-1\n@SP\nM=M+1\n(ELSE{idx})\n'
 
+
 def getBinOpCode(op):
     return f"@SP\nM=M-1\nA=M\nD=M\n@SP\nM=M-1\nA=M\n{op}\n@SP\nA=M\nM=D\n@SP\nM=M+1\n"
 
+
 def getUniOpCode(op):
     return f"@SP\nM=M-1\nA=M\nD=M\n@SP\nA=M\n{op}\n@SP\nM=M+1\n"
+
+
+def getMainSegPushCode(seg):
+    return f"D=A\n@{seg}\nD=D+M\nA=D\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n"
+
+
+def getMainSegPopCode(seg, i):
+    return f"@{i}\nD=A\n@{seg}\nD=D+M\n@i\nM=D\n@SP\nM=M-1\n@SP\nA=M\nD=M\n@i\nA=M\nM=D\n"
+
+
+def getPointerPushCode(base):
+    return f"@{base}\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n"
+
+
+def getPointerPopCode(base):
+    return f"@SP\nM=M-1\nA=M\nD=M\n@{base}\nM=D\n"
 
 class CommandType(Enum):
     C_ARITHMETIC = 0
@@ -66,6 +84,8 @@ class Parser:
             return CommandType.C_PUSH
         elif curArr[0] in ARITHMETIC_OPS:
             return CommandType.C_ARITHMETIC
+        elif curArr[0] == "pop":
+            return CommandType.C_POP
 
     def getFirstArg(self, tp) -> str:
         if tp == CommandType.C_ARITHMETIC:
@@ -75,7 +95,7 @@ class Parser:
 
     def getSecondArg(self, tp) -> int:
         if tp == CommandType.C_PUSH or tp == CommandType.C_POP or tp == CommandType.C_FUNCTION or tp == CommandType.C_CALL:
-            return self.curArr[2]
+            return int(self.curArr[2])
 
 
 # writes the assembly code that implements the parsed command
@@ -110,12 +130,60 @@ class CodeWriter:
         elif firstArg == "not":
             self.f.write(getUniOpCode("M=!D"))
 
-    def writePushPop(self) -> None:
+    def writePush(self, filename) -> None:
         parser = self.parser
-        if parser.getFirstArg(tp) == "constant":
-            i = parser.getSecondArg(tp)
+        firstArg = parser.getFirstArg(tp)
+        i = parser.getSecondArg(tp)
+        if firstArg == "constant":
             self.f.write(f"@{i}\n")
             self.f.write("D=A\n@SP\nA=M\nM=D\n@SP\nM=M+1\n")
+        elif firstArg == "local":
+            self.f.write(f"@{i}\n")
+            self.f.write(getMainSegPushCode("LCL"))
+        elif firstArg == "argument":
+            self.f.write(f"@{i}\n")
+            self.f.write(getMainSegPushCode("ARG"))
+        elif firstArg == "this":
+            self.f.write(f"@{i}\n")
+            self.f.write(getMainSegPushCode("THIS"))
+        elif firstArg == "that":
+            self.f.write(f"@{i}\n")
+            self.f.write(getMainSegPushCode("THAT"))
+        elif firstArg == "temp":
+            self.f.write(f"@{i}\n")
+            self.f.write(
+                f"@{i}\nD=A\n@5\nD=D+A\nA=D\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n")
+        elif firstArg == "static":
+            self.f.write(f"@{filename}.{i}\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n")
+        elif firstArg == "pointer":
+            if i == 0:
+                self.f.write(getPointerPushCode("THIS"))
+            elif i == 1:
+                self.f.write(getPointerPushCode("THAT"))
+
+    def writePop(self, filename) -> None:
+        parser = self.parser
+        firstArg = parser.getFirstArg(tp)
+        i = parser.getSecondArg(tp)
+        if firstArg == "local":
+            self.f.write(getMainSegPopCode("LCL", i))
+        elif firstArg == "argument":
+            self.f.write(getMainSegPopCode("ARG", i))
+        elif firstArg == "this":
+            self.f.write(getMainSegPopCode("THIS", i))
+        elif firstArg == "that":
+            self.f.write(getMainSegPopCode("THAT", i))
+        elif firstArg == "temp":
+            self.f.write(
+                f"@{i}\nD=A\n@5\nD=D+A\n@i\nM=D\n@SP\nM=M-1\n@SP\nA=M\nD=M\n@i\nA=M\nM=D\n")
+        elif firstArg == "static":
+            self.f.write(f"@SP\nM=M-1\nA=M\nD=M\n@{filename}.{i}\nM=D\n")
+        elif firstArg == "pointer":
+            if i == 0:
+                self.f.write(getPointerPopCode("THIS"))
+            elif i == 1:
+                self.f.write(getPointerPopCode("THAT"))
+
 
     def writeEnd(self) -> None:
         self.f.write("(INFINITE_LOOP)\n@INFINITE_LOOP\n0;JMP")
@@ -125,12 +193,14 @@ class CodeWriter:
 
 
 if __name__ == "__main__":
-    import sys
-    filename = sys.argv[1:][0]
-    destname = filename.replace(".vm", ".asm")
+    import sys, os
+    filepath = sys.argv[1:][0]
+    destpath = filepath.replace(".vm", ".asm")
+
+    filename = os.path.basename(filepath)
 
     parser = Parser(sys.argv[1:][0])
-    codeWriter = CodeWriter(destname, parser)
+    codeWriter = CodeWriter(destpath, parser)
 
     while parser.hasMoreLines():
         parser.advance()
@@ -139,6 +209,8 @@ if __name__ == "__main__":
         if tp == CommandType.C_ARITHMETIC:
             codeWriter.writeArithmethic()
         elif tp == CommandType.C_PUSH:
-            codeWriter.writePushPop()
+            codeWriter.writePush(filename[:-3])
+        elif tp == CommandType.C_POP:
+            codeWriter.writePop(filename[:-3])
     codeWriter.writeEnd()
     codeWriter.close()
